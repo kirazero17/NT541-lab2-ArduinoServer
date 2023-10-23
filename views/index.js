55eaf7c9c420746b0eb9b5994ced6c058e5e9e48
Long T. Kira <longdragon02@gmail.com>
2023-10-23 22:49:28 +0700

    update

---
+++ views/index.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+/** @param {Event} event */
+function handleSubmit(event) {
+  // The rest of the logic will go here.
+}
